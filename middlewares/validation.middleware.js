ef3d141fd42da02cd9c4114d0239e98d3c237a2d
Andres1717 <mariaerq10@gmail.com>
2023-04-17 18:51:36 -0500

    Creado el validation.middleware, terminada la validacion del create user. (Falta provarla)

---
+++ middlewares/validation.middleware.js
@@ -0,0 +1,32 @@
+const { body, validationResult } = require('express-validator');
+
+//VALIDACION DE PROPIEDADES
+const validfields = (req, res, next) => {
+  const errors = validationResult(req);
+
+  if (errors.isEmpty()) {
+    return res.status(400).json({
+      status: 'error',
+      errors: errors.mapped(),
+    });
+  }
+
+  next();
+};
+
+exports.createUserValidation = [
+  body('name').notEmpty().withMessage.apply('Name cannot be empty'),
+  body('email')
+    .notEmpty()
+    .withMessage('email cannot be empty')
+    .isEmail()
+    .withMessage('Must be a valid email'),
+  body('password').notEmpty().isLength({ min: 8, max: 20 }).withMessage('password cannot be empty').withMessage('password must be at least 8 characters and maximum 20'),
+  validfields()
+];
+
+/**
+ * RECUERDE PROVAR ESTA VALIDACION ANTES DE HACER LAS OTRAS
+ * Los campos que se deben validar son lo que traen informacion
+ * que envia el cliente
+ */
