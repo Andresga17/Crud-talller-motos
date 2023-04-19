ad8e6f6117e7e6503cf16400e1284ba61de9353b
Andres1717 <mariaerq10@gmail.com>
2023-04-18 21:23:13 -0500

    globalErrorHandler funcionando

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const morgan = require('morgan');
 const userRouter = require('./routes/user.routes');
 const repairRouter = require('./routes/repair.routes');
+
 const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/error.controller');
 
 const app = express();
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 app.use(express.json());
 
@@ -15,5 +22,7 @@ app.all('*', (req, res, next) => {
     new AppError(`Can't find ${req.originalUrl} on this server`, 404)
   );
 });
+
+app.use(globalErrorHandler)
 
 module.exports = app;
